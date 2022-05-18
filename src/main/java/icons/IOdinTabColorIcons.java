61444ba7edc00129d90fa3e78571e50afba8409a
dingjijun <dingjijun@aecg.com.cn>
2022-05-18 19:22:53 +0800

    feat: build tabColor plugin

---
+++ src/main/java/icons/IOdinTabColorIcons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface IOdinTabColorIcons {
+    Icon TabColor_Red = IconLoader.getIcon("/META-INF/pluginIcon_tabColor_red_dark.svg",IOdinTabColorIcons.class);
+}
